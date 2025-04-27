f74bd05176436d4358b133411546eccddadfd7b9
Sythe <sythe@gmail.com>
2025-04-27 09:54:17 +0530

    sql change to simple

--- todo_list.sql
+++ todo_list.sql
@@ -1,62 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 26, 2025 at 03:47 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.0.32
+CREATE DATABASE IF NOT EXISTS todo_list;
+USE todo_list;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `todo_list`
---
-CREATE DATABASE IF NOT EXISTS `todo_list` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `todo_list`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tasks`
---
-
-CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL,
-  `task` varchar(255) NOT NULL,
-  `status` enum('pending','completed') NOT NULL DEFAULT 'pending'
+CREATE TABLE IF NOT EXISTS tasks (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  task VARCHAR(255) NOT NULL,
+  status ENUM('pending', 'completed') NOT NULL DEFAULT 'pending',
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tasks`
---
-ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tasks`
---
-ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
